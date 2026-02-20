4949cd6c21c87ac1be9dc4b44660b5eca374b4f8
Ashraf0907 <skashraf1025@gmail.com>
2026-02-20 09:36:35 +0530

    Base application setup

---
+++ src/UC1PalindromeCheckerApp.java
@@ -0,0 +1,6 @@
+public class UC1PalindromeCheckerApp {
+    public static void main (String args[] ){
+        System.out.println("Welcome to Palindrome Checker Management System\nVersion : 1.0\nSystem initialized successfully");
+
+    }
+}

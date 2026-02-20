1cc15e47d801f4981ed34b68360cc3a335face13
Ashraf0907 <skashraf1025@gmail.com>
2026-02-20 10:06:07 +0530

    UC2:Hardcoded palindrome

--- src/UC1PalindromeCheckerApp.java
+++ src/UC1PalindromeCheckerApp.java
@@ -1,6 +1,25 @@
-public class UC1PalindromeCheckerApp {
-    public static void main (String args[] ){
-        System.out.println("Welcome to Palindrome Checker Management System\nVersion : 1.0\nSystem initialized successfully");
 
-    }
-}
+        import java.util.Scanner;
+
+        public class UC1PalindromeCheckerApp {
+            public static void main(String[] args) {
+                Scanner sc = new Scanner(System.in);
+
+                System.out.print("Enter a string: ");
+                String str = sc.nextLine();
+
+                boolean isPalindrome = true;
+
+                for (int i = 0; i < str.length() / 2; i++) {
+                    if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+                        isPalindrome = false;
+                        break;
+                    }
+                }
+
+                System.out.println("Is it a palindrome? : " + isPalindrome);
+
+                sc.close();
+            }
+        }
+
